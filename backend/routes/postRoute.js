6bf55bd0b822bef284fc9444ae7a584399e7ea26
Vikas Rathore <vikasrathore160@gmail.com>
2026-08-09 09:43:48 +0530

    fix: format code for consistency in postRoute.js

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -12,33 +12,24 @@ import {
 import { protect } from "../middleware/authMiddleware.js";
 import fileUpload from "../middleware/upload.js";
 
-
 const router = express.Router();
 
-
 // CREATE POST
-router.post("/",protect,fileUpload,postCreate);
-
+router.post("/", protect, fileUpload, postCreate);
 
 // UPDATE POST
-router.put("/:id",protect,fileUpload,updatePost);
-
+router.put("/:id", protect, fileUpload, updatePost);
 
 // DELETE POST
-router.delete("/:id",protect,deletePost);
-
+router.delete("/:id", protect, deletePost);
 
 // GET ALL POSTS
 router.get("/", getAllPsot);
 
-
 // TOP POSTS
 router.get("/top", getTopRecentPosts);
 
-
-// SINGLE POST
-router.get("/:id", getPostbyId
-);
-
+// GET SINGLE POST
+router.get("/:id", getPostbyId);
 
 export default router;

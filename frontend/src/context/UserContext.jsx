3e46b1975c53ec079a817957bf262c744d4fc848
Vikas Rathore <149600680+Vikas123Rathore@users.noreply.github.com>
2026-08-09 08:44:58 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -9,7 +9,7 @@ export const UserProvider = ({ children }) => {
   const [error, setError] = useState("");
 
   // Backend URL
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://mern-integeration-backend.onrender.com/";
 
   // ================= GET CURRENT USER =================
 
